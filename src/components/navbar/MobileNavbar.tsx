866652e0a207d994ed6146c24eb39408c82b434a
keiyamagata <kei.yamagata@icloud.com>
2024-03-11 14:49:23 +0100

    remove: unused comment

--- src/components/navbar/MobileNavbar.tsx
+++ src/components/navbar/MobileNavbar.tsx
@@ -29,19 +29,6 @@ export const MobileNavbar = () => {
         }`}
         aria-label="menu"
       >
-        {/* <div className="flex justify-end">
-          <button
-            data-collapse-toggle="mobile-menu"
-            type="button"
-            className="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="mobile-menu"
-            aria-expanded="false"
-            onClick={() => setIsOpen(false)}
-          >
-            <span className="sr-only">Close main menu</span>
-            <XIcon />
-          </button>
-        </div> */}
         <ul
           id="mobile-menu"
           className="mt-4 flex w-full flex-col gap-2 text-right"
